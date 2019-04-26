d102f8b6a3cbae6ffea4902da2c4ad13a1fb385a
dantoking <dantoking@gmail.com>
2019-04-26 20:36:52 +0700

    first commit

---
+++ adet.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>COBASSa</title>
+		<style type="text/css">
+			p {
+				color: red;
+				font-family: 'arial';
+			}
+			/*
+			*/
+		</style>
+	</head>
+	
+	<body>
+		<!-- UTAMANYA -->
+		<h1>CARA</h1>
+		<h2>CARA</h2>
+		<h3>CARA</h3>
+
+		<p >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		<br>
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		<hr>
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+		<ol> <!-- COMMENT :  INI ORDER LIST untuk list nomer/ huruf-->
+			<li>
+				satu
+			</li>
+			<li>
+				dua
+			</li>
+		</ol>
+		<ul>
+			<li>TANDA </li>
+		</ul>
+
+		<table border="1">
+			<tr> 
+				<!-- KOLOM-->
+				<td>	<!-- BARIS -->	BARIS 1 KOLOM 1	</td>
+				<td>	<!-- BARIS -->	BARIS 2 KOLOM 1	</td>
+				<td>	<!-- BARIS -->	BARIS 2 KOLOM 1	</td>
+
+			</tr>
+			<tr>
+				<td>
+					BAWAH
+				</td>
+			</tr>
+
+		</table>
+		<marquee behavior="scroll" direction="left">Your scrolling text goes here</marquee>
+			<script type="text/javascript">
+				alert("HALO ADIT");
+			</script>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<?php include 'head.php'; ?>
+<?php if (empty($_SESSION)): // header('Location: login.php'); ?>
+<?php endif ?>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+<?php define('BASEURL', '/bengkel'); ?>
+
+<?php include 'header.php'; ?>
+<?php include 'sidebar.php'; ?>
+
+  <div class="content-wrapper">
+<?php 
+
+require 'conn.php';
+
+// echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+// echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+
+if (isset($_GET['page'])) {
+  if ($_GET['page']=='pegawai') {
+  include 'page/pegawai.php';
+  }else if ($_GET['page']=='tambah-pegawai') {
+  include 'page/tambah-pegawai.php';
+}else if ($_GET['page']=='del-pegawai') {
+  include 'page/del-pegawai.php';
+  }else if ($_GET['page']=='pelanggan') {
+  include 'page/pelanggan.php';
+  }else if ($_GET['page']=='tambah-pelanggan') {
+  include 'page/tambah-pelanggan.php';
+  }else if ($_GET['page']=='supplier') {
+  include 'page/supplier.php';
+  }else if ($_GET['page']=='tambah-supplier') {
+  include 'page/tambah-supplier.php';
+  }else if ($_GET['page']=='produk') {
+  include 'page/produk.php';
+  }else if ($_GET['page']=='tambah-produk') {
+  include 'page/tambah-produk.php';
+  }else if ($_GET['page']=='jasa') {
+  include 'page/jasa.php';
+  }else if ($_GET['page']=='tambah-jasa') {
+  include 'page/tambah-jasa.php';
+  }else if ($_GET['page']=='pengiriman') {
+  include 'page/pengiriman.php';
+  }else if ($_GET['page']=='tambah-pengiriman') {
+  include 'page/tambah-pengiriman.php';
+}else if ($_GET['page']=='transaksi') {
+  include 'page/transaksi.php';
+  }else if ($_GET['page']=='tambah-transaksi') {
+  include 'page/tambah-transaksi.php';
+
+  }
+}else{
+  
+  include 'content.php';
+  
+
+  }
+ ?>
+  
+  </div>
+  </div>
+
+  <!-- Content Wrapper. Contains page content -->
+  
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.4.0
+    </div>
+    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    reserved.
+
+    <strong>Copyright &copy; 2019 <a href="https://adminlte.io">HARDANTO P</a>.</strong>   </footer>
+
+  <!-- Control Sidebar -->
+  
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+<?php include 'jsbawah.php'; ?>
+<?php mysqli_close($link); ?>
+
+</body>
+</html>
